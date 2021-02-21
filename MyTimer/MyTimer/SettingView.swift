ddc31415fcbd3e049851cd0257295753092c2637
Shusaku-K <77326356+Shusaku-K@users.noreply.github.com>
2021-02-21 15:35:57 +0900

    Update SettingView.swift

--- MyTimer/MyTimer/SettingView.swift
+++ MyTimer/MyTimer/SettingView.swift
@@ -3,22 +3,25 @@
 //  MyTimer
 //
 //  Created by 金城秀作 on 2021/02/14.
-//
+//  SettingViewの呼び出しはContentViewの92行目を確認。
 
 import SwiftUI
 
 struct SettingView: View {
     
+    // 永続化する秒数設定（初期値は10
     @AppStorage("timer_value") var timerValue = 10
     
 
     
     var body: some View {
+        //奥行き（背景の設定）
         ZStack {
-            
+            // 背景色表示
             Color("backgroundSetting")
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             
+            //垂直にレイアウト(テキストを縦並びに表示)
             VStack {
                 
                 Spacer()
@@ -26,6 +29,7 @@ struct SettingView: View {
                     .font(.largeTitle)
                 Spacer()
                 
+                 // Pickerを表示（秒数を変えたい場合はここを変更）
                 Picker(selection: $timerValue, label: Text("選択")) {
                     Text("10")
                         .tag(10)
